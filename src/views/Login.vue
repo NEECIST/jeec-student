ca71dff9232a56a41195071a7ac02e93b4c862d3
jomivaan <jomivaan@gmail.com>
2023-10-15 22:19:25 +0100

    works, need to get info now, with JWT 3rd try

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,7 +5,7 @@
         <img alt="JEEC logo" src="../assets/jeec_colour_no_edition.svg" />
       </div>
       <div>
-        V1.3
+        V1.4
       </div>
       <div class="buttons-flex" v-if="!loading">
         
