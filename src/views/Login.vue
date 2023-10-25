805f84378267724e717c6c11609656d7dc9984aa
dark66jedi <111192983+dark66jedi@users.noreply.github.com>
2023-10-25 22:38:45 +0100

    Update Login.vue axios endpoint

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,7 +5,7 @@
         <img alt="JEEC logo" src="../assets/jeec_colour_no_edition.svg" />
       </div>
       <div>
-        V1.6
+        V1.7
       </div>
       <div class="buttons-flex" v-if="!loading">
         
@@ -145,7 +145,7 @@ export default {
         visitor.append('email_validation', parserJwt.parseJwt(response.credential).email_verified)
         visitor.append('picture', parserJwt.parseJwt(response.credential).picture)
         
-        axios.post(process.env.VUE_APP_JEEC_BRAIN_URL+"/redirect_uri_google",visitor).then(response => this.responsedata = response.data)
+        axios.post(process.env.VUE_APP_JEEC_BRAIN_URL+"/student/redirect_uri_google",visitor).then(response => this.responsedata = response.data)
       },
     
     decrypt(code) {
